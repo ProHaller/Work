96ce93fd417c3c50931312cffd7ab8245efb85fd
ProHaller <88140523+ProHaller@users.noreply.github.com>
2023-12-18 12:37:31 +0900

    FEATURE - Relationship displayed in color and labeled.

--- employee_graph.py
+++ employee_graph.py
@@ -16,22 +16,65 @@ def create_graph(data):
         G.add_node(employee["name"], attr=employee)
         for colleague in data["employees"]:
             if employee["name"] != colleague["name"]:
-                # Calculate similarity or "distance" based on shared attributes
-                distance = calculate_distance(employee, colleague)
-                if distance > 0:
-                    G.add_edge(employee["name"], colleague["name"], weight=distance)
+                # Attributes for edge types
+                edge_attrs = {
+                    "shared_projects": len(
+                        set(employee["projects"]) & set(colleague["projects"])
+                    ),
+                    "same_team": len(set(employee["teams"]) & set(colleague["teams"])),
+                    "same_location": employee["office_location"]
+                    == colleague["office_location"],
+                }
+                # Only add an edge if there is some shared attribute
+                if any(edge_attrs.values()):
+                    G.add_edge(employee["name"], colleague["name"], **edge_attrs)
     return G
-
-
-def calculate_distance(emp1, emp2):
-    # Simple example: count of shared projects
-    shared_projects = len(set(emp1["projects"]) & set(emp2["projects"]))
-    return shared_projects
 
 
 def visualize_graph(G):
     pos = nx.spring_layout(G)  # Using spring layout for visualization
-    nx.draw(G, pos, with_labels=True)
+
+    # Drawing nodes
+    nx.draw_networkx_nodes(G, pos)
+
+    # Drawing edges with different styles for each relationship type
+    shared_projects_edges = [
+        (u, v) for u, v, d in G.edges(data=True) if d["shared_projects"] > 0
+    ]
+    same_team_edges = [(u, v) for u, v, d in G.edges(data=True) if d["same_team"] > 0]
+    same_location_edges = [
+        (u, v) for u, v, d in G.edges(data=True) if d["same_location"]
+    ]
+
+    nx.draw_networkx_edges(
+        G,
+        pos,
+        edgelist=shared_projects_edges,
+        edge_color="blue",
+        label="Shared Projects",
+    )
+    nx.draw_networkx_edges(
+        G,
+        pos,
+        edgelist=same_team_edges,
+        edge_color="green",
+        style="dashed",
+        label="Same Team",
+    )
+    nx.draw_networkx_edges(
+        G,
+        pos,
+        edgelist=same_location_edges,
+        edge_color="red",
+        style="dotted",
+        label="Same Location",
+    )
+
+    # Drawing labels
+    nx.draw_networkx_labels(G, pos)
+
+    # Show legend
+    plt.legend()
     plt.show()
 
 
